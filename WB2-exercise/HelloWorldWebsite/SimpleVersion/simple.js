8530fb08cc669b6c009bdd37b7599dcfa8246a98
Student <sherpatashi50@gmail.com>
2022-10-20 14:27:03 -0400

    added Greetname

---
+++ WB2-exercise/HelloWorldWebsite/SimpleVersion/simple.js
@@ -0,0 +1,22 @@
+"use strict";
+
+window.onload = Init;
+
+
+function init()
+{
+    const calculatebotton = document.getElementById("calculateButton");
+    calculatebotton.onclick = calculatebuttonclick;
+}
+
+function calculatebuttonclick()
+{
+    //do the calculate
+
+    const number1Textbox = document.getElementById("calculateButton");
+
+    Let nmsdekn
+    const number2Textbox = document.getElementById("calculateButton);
+
+}
+
